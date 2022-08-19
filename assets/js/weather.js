87d237897796c17b2ec4165d19e61a9e82ab0780
paul lovato <lovato.paul97@yahoo.com>
2022-08-18 22:10:46 -0400

    displayWeather function completed. added weather and city name

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,4 +1,3 @@
-var weatherCall = "";
 var cityEl  = document.querySelector("#city")
 var searchEl = document.querySelector("#search-btn")
 var cityDate = document.querySelector("#cityDate")
@@ -12,15 +11,18 @@ fetch(apiUrl)
     })
     .then(function (data) {
         console.log(data);
+        displayWeather(data);
+
 var uvIndex = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=8a42d43f7d7dc180da5b1e51890e67dc`
         return fetch(uvIndex)
     })
     .then(function (res) {
         return res.json();
     })
-    .then(function (data) {
-        console.log(data);
+    .then(function (dataUv) {
+        console.log(dataUv);
     });
+
 };
 
 var citySearch = function(event) {
@@ -29,12 +31,32 @@ var citySearch = function(event) {
     var searchTerm = cityEl.value.trim();
 
     if (searchTerm) {
-        weatherCall(searchTerm)
-        searchTerm = "";
-    }else console.warn("you must chose a city");
+        weatherCall(searchTerm);
+        cityEl.value = "";
+    } else
+     window.alert("You must chose a city");
+}
 
+var displayWeather = function(data) {
+var cityDisplay = {
+    name:data.name,
+};
 
+    var searchValid = document.createElement("span");
+        searchValid.textContent = cityDisplay.name;
+        searchValid.classList.add("d-flex", "card-title", "justify-content-center", "justify-content-between", "shadow-sm", "weather-title");
+        cityDate.append(searchValid);
+    
+        var timeSet = document.createElement("h4")
+        var currentDate = data.dt;
+        var date = new Date(currentDate * 1000);
+        var month = date.getMonth();
+        var day = date.getDate();
+        var year = date.getFullYear();
+        var forecastFormattedTime = parseInt(month) + 1 + "/" + day + "/" + year;
+        timeSet.textContent = forecastFormattedTime;
+        searchValid.append(timeSet)
 
 }
 
-searchEl.addEventListener("click", citySearch)
+searchEl.addEventListener("click", citySearch);
